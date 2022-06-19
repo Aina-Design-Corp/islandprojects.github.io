735f4d8622f074310f08435e720fa81d27c45a36
Joe Hagedorn <joe.hagedorn@ainadesign.org>
2022-06-18 20:24:41 -1000

    added school listing

---
+++ services/xlsxtranspose.js
@@ -0,0 +1,33 @@
+const xlsx = require("xlsx");
+var fs = require("fs");
+
+convertExcelFileToJsonUsingXlsx();
+
+function convertExcelFileToJsonUsingXlsx() {
+    // Read the file using pathname
+    const file = xlsx.readFile('../data/SchoolList.xlsx');
+    // Grab the sheet info from the file
+    const sheetNames = file.SheetNames;
+    const totalSheets = sheetNames.length;
+    // Variable to store our data 
+    let parsedData = [];
+    // Loop through sheets
+    for (let i = 0; i < totalSheets; i++) {
+        // Convert to json using xlsx
+        const tempData = xlsx.utils.sheet_to_json(file.Sheets[sheetNames[i]]);
+        // Skip header row which is the colum names
+        tempData.shift();
+        // Add the sheet's json to our data array
+        parsedData.push(...tempData);
+    }
+ // call a function to save the data in a json file
+ generateJSONFile(parsedData);
+}
+
+function generateJSONFile(data) {
+    try {
+        fs.writeFileSync('SchoolList.json', JSON.stringify(data))
+    } catch (err) {
+        console.error(err)
+    }
+}
